517d076fcdb5a98a6b04096a606cfe26a89e29b7
Juan Camilo Forero <juforeroca@unal.edu.co>
2025-10-21 20:05:50 -0500

    Delete Unique_Module directory

--- Unique_Module/_init_.py
+++
@@ -1,6 +0,0 @@
-from .shapes import Point, Line, Shape, Rectangle, Square, Triangle 
-                     
-from .shapes import EquilateralTriangle, ScaleneTriangle, IsoscelesTriangle, RightTriangle
-                     
-
-
