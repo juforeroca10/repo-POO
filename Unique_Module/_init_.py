76f54d36f65fb073c301dcfc22b2538de203dc9f
juforeroca10 <juforeroca@unal.edu.co>
2025-10-21 19:38:49 -0500

    Reto 5 - Unique Module

---
+++ Unique_Module/_init_.py
@@ -0,0 +1,6 @@
+from .shapes import Point, Line, Shape, Rectangle, Square, Triangle 
+                     
+from .shapes import EquilateralTriangle, ScaleneTriangle, IsoscelesTriangle, RightTriangle
+                     
+
+
